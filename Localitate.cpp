487160839dccf6dc2d1bf13d56aa7a2fd1e923e1
andidroid2000 <80198989+andidroid2000@users.noreply.github.com>
2021-03-16 23:14:14 +0200

    Add files via upload

---
+++ Localitate.cpp
@@ -0,0 +1,6 @@
+//
+// Created by andy2 on 16.03.2021.
+//
+
+#include "Localitate.h"
+
---
+++ Localitate.h
@@ -0,0 +1,15 @@
+//
+// Created by andy2 on 16.03.2021.
+//
+
+#ifndef PROIECT_POO_LOCALITATE_H
+#define PROIECT_POO_LOCALITATE_H
+#include <iostream>
+
+class Localitate {
+private:
+    std::string nume;
+};
+
+
+#endif //PROIECT_POO_LOCALITATE_H
