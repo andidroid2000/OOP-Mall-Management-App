fb622e557eafb6700a18fcf7b0c58d6f50fe9a71
andidroid2000 <80198989+andidroid2000@users.noreply.github.com>
2021-03-19 00:33:19 +0200

    Add files via upload

---
+++ Festivaluri.cpp
@@ -0,0 +1,5 @@
+//
+// Created by andy2 on 18.03.2021.
+//
+#include <iostream>
+#include "Festivaluri.h"
---
+++ Festivaluri.h
@@ -0,0 +1,15 @@
+//
+// Created by andy2 on 18.03.2021.
+//
+#ifndef PROIECT_POO_FESTIVALURI_H
+#define PROIECT_POO_FESTIVALURI_H
+#include <iostream>
+#include <string>
+
+class Festivaluri {
+private:
+    std::string nume;
+    std::string perioada_desfasurare;
+};
+
+#endif //PROIECT_POO_FESTIVALURI_H
