64196f7f10e9075b47e85a558cf3867bd35baae2
andidroid2000 <80198989+andidroid2000@users.noreply.github.com>
2021-03-19 00:30:14 +0200

    Update Localitate.cpp

--- Localitate.cpp
+++ Localitate.cpp
@@ -1,22 +1,5 @@
 //
 // Created by andy2 on 16.03.2021.
 //
-#ifndef PROIECT_POO_LOCALITATE_H
-#define PROIECT_POO_LOCALITATE_H
 #include <iostream>
-#include <string>
-
-class Localitate{
-private:
-    std::string nume;
-    std::string judet;
-    int an_fondare;
-    int numar_turisti;
-public:
-    Localitate(){};
-    void localitate_noua( const std::string &nume, const std::string &judet, const int an_fondare);
-    ~Localitate(){};
-};
-
-
-#endif //PROIECT_POO_LOCALITATE_H
+#include "Localitate.h"
