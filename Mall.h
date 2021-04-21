f7aa3c8b0ad61d678efd75287ec02c5f9e024cd6
andidroid2000 <80198989+andidroid2000@users.noreply.github.com>
2021-04-21 12:46:47 +0300

    Add files via upload

--- Mall.h
+++ Mall.h
@@ -1,58 +1,73 @@
 #pragma once
 #include "Magazine.h"
 #include "Jobs.h"
+#include <vector>
+
 
 class Mall
 {
 private:
-	SpatiuComercial** locatii;
-	Personal** angajati;
+	std::vector<SpatiuComercial*> locatii;
+	//SpatiuComercial** locatii;
+	std::vector<Personal*> angajati;
+	//Personal** angajati;
 	int numar_locatii, numar_angajati;
 public:
 	Mall(int numar_locatii, int numar_angajati)
 	{
 		this->numar_locatii = 0;
 		this->numar_angajati = 0;
-		locatii = new SpatiuComercial * [numar_locatii];
-		angajati = new Personal * [numar_angajati];
+		//locatii = new SpatiuComercial * [numar_locatii];
+		//angajati = new Personal * [numar_angajati];
 	}
 
 	~Mall()
 	{
 		numar_locatii = 0;
 		numar_angajati = 0;
-		delete[]locatii;
-		delete[]angajati;
+		//delete[]locatii;
+		//delete[]angajati;
 	}
 
 	void adaugare_magazin(Restaurant locatie)
 	{
-		locatii[this->numar_locatii++] = new Restaurant(locatie);
+		locatii.push_back(new Restaurant(locatie));
+		//locatii[this->numar_locatii++] =;
 	}
 
 	void adaugare_magazin(Moda locatie)
 	{
-		locatii[this->numar_locatii++] = new Moda(locatie);
+		locatii.push_back(new Moda(locatie));
+		//locatii[this->numar_locatii++] = new Moda(locatie);
 	}
 
 	void adaugare_angajati(Manager angajat)
 	{
-		angajati[this->numar_angajati++] = new Manager(angajat);
+		Manager* man;
+
+		man = new Manager(angajat);
+		if (man == nullptr)
+			throw std::bad_alloc();
+
+		angajati.push_back(man);
+		//angajati[this->numar_angajati++] = new Manager(angajat);
 	}
 
 	void adaugare_angajati(Vanzator angajat)
 	{
-		angajati[this->numar_angajati++] = new Vanzator(angajat);
+		angajati.push_back(new Vanzator(angajat));
+		//angajati[this->numar_angajati++] = new Vanzator(angajat);
 	}
 
 	void adaugare_angajati(Paznic angajat)
 	{
-		angajati[this->numar_angajati++] = new Paznic(angajat);
+		angajati.push_back(new Paznic(angajat));
+		//angajati[this->numar_angajati++] = new Paznic(angajat);
 	}
 
 	void afisare_locatii()
 	{
-		for (int i = 0; i < numar_locatii; i++)
+		for (int i = 0; i < locatii.size(); i++)
 		{
 			//std::cout << locatii[i] << "\n\n";
 			locatii[i]->afisare();
@@ -61,7 +76,7 @@ public:
 
 	void afisare_angajati()
 	{
-		for (int i = 0; i < numar_angajati; i++)
+		for (int i = 0; i < angajati.size(); i++)
 		{
 			//std::cout << angajati[i] << "\n\n";
 			angajati[i]->afisare_personal();
