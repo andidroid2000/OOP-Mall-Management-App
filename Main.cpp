1c9449f9db50b70d81781dd6af1ec5887edcd9a3
andidroid2000 <80198989+andidroid2000@users.noreply.github.com>
2021-05-16 15:39:14 +0300

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -5,29 +5,44 @@
 
 int main()
 {
-    Restaurant restaurant1("KFC", 1, 2020, 1);
-    restaurant1.afisare();
-    Moda moda("H&M", 0, 2021, 0);
-    std::cout << moda;
+    Restaurant restaurant1 ("KFC", 1, 2020, 1), restaurant2 ("Chopstix", 1, 2021, 0), restaurant3 ("McDonald's", 1 ,2015, 6);
+    Moda magazin1("H&M", 0, 2021, 0), magazin2("New Yorker", 0, 2020, 1);
+    
     Manager manager1("George", "Georgel", 2015);
+    try 
+    {
+        manager1.verificare_an();
+    }
+    catch (const std::exception& verificare)
+    {
+        std::cout << verificare.what();
+    }
     manager1.afisare_personal();
     manager1.ajustari_salariu();
 
-     Mall mall(2, 3);
-     mall.adaugare_magazin(restaurant1);
-     mall.adaugare_magazin(moda);
+    Vanzator vanzator1("Dan", "Daniel", 2021);
+    try
+    {
+        vanzator1.verificare_an();
+    }
+    catch (const std::exception& verificare)
+    {
+        std::cout << verificare.what();
+    }
+    vanzator1.afisare_personal();
+    vanzator1.ajustari_salariu();
 
-     try
-     {
-         mall.adaugare_angajati(manager1);
-     }
-     catch ( std::bad_alloc& exceptie )
-     {
-         std::cerr << "Avem o exceptie: " << exceptie.what() << std::endl;
-     }
-    
+    Mall mall(3, 3);
+    mall.adaugare_magazin(restaurant1);
+    mall.adaugare_magazin(restaurant2);
+    mall.adaugare_magazin(restaurant3);
+    mall.adaugare_magazin(magazin1);
+    mall.adaugare_magazin(magazin2);
 
-     mall.afisare_locatii();
-     mall.afisare_angajati();
-  
+    mall.adaugare_angajati(manager1);
+    mall.adaugare_angajati(vanzator1);
+    std::cout << "====================================================<< Restaurante >>========================================================\n";
+    mall.afisare_locatii();
+    std::cout << "======================================================<< Angajati >>=========================================================\n";
+    mall.afisare_angajati();
 }
